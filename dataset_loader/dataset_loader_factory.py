2d084a62fac6d8cc6f584296d15f749adb3641af
Mauricio Sightman <mauriciosightman@gmail.com>
2025-07-25 11:19:55 -0300

    Refactor dataset loader imports to improve readability and maintainability

--- dataset_loader/dataset_loader_factory.py
+++ dataset_loader/dataset_loader_factory.py
@@ -1,10 +1,3 @@
-from dataset_loader.afd_data_loader import AFDDataLoader
-from dataset_loader.cdr_data_loader import CDRDataLoader
-from dataset_loader.lenerbr_data_loader import LeNERbrDataLoader
-from dataset_loader.conll2003_data_loader import Conll2003DataLoader
-from dataset_loader.ter_otonotes5_data_loader import Ontonotes5DataLoader
-from dataset_loader.ai4privacy_pii_data_loader import Ai4privacyPiiDataLoader
-
 class DatasetLoaderFactory:
     """
     Base class for dataset factories.
@@ -19,21 +12,33 @@ class DatasetLoaderFactory:
 
     def get_dataset_loader(self, logger):
         if self.dataset_name == 'Ai4privacyPii':
+            from dataset_loader.ai4privacy_pii_data_loader import Ai4privacyPiiDataLoader
+
             return Ai4privacyPiiDataLoader(self.config, logger)
         
         if self.dataset_name == 'Conll2003':
+            from dataset_loader.conll2003_data_loader import Conll2003DataLoader
+            
             return Conll2003DataLoader(self.config, logger)
         
         if self.dataset_name == 'AFD':
+            from dataset_loader.afd_data_loader import AFDDataLoader
+            
             return AFDDataLoader(self.config, logger)
         
         if self.dataset_name == 'CDR':
+            from dataset_loader.cdr_data_loader import CDRDataLoader
+            
             return CDRDataLoader(self.config, logger)
         
         if self.dataset_name == 'LeNERbr':
+            from dataset_loader.lenerbr_data_loader import LeNERbrDataLoader
+            
             return LeNERbrDataLoader(self.config, logger)
         
         if self.dataset_name == 'Ontonotes5':
+            from dataset_loader.ter_otonotes5_data_loader import Ontonotes5DataLoader
+
             return Ontonotes5DataLoader(self.config, logger)
 
         else:
